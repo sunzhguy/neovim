ccbe53f9097efff069ae5dea8a9d98dfe065b151
sunzhguy <sunzhigang1@kylinos.cn>
2022-08-21 23:04:21 -0700

    add plugs treesitter support

--- lua/plugins-config.lua
+++ lua/plugins-config.lua
@@ -1,5 +1,4 @@
-local status, nvim_tree = pcall(require, "nvim-tree")
-if not status then
+local status, nvim_tree = pcall(require, "nvim-tree") if not status then
   vim.notify("没有找到 nvim-tree")
   return 
 end
@@ -200,3 +199,59 @@ telescope.setup({
   pcall(telescope.load_extension, "ui-select")
 
 
+local status, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status then
+  vim.notify("没有找到 treesitter")
+  return 
+end
+
+
+treesitter.setup({
+  --安装language parser
+  -- :TSInstallInfo 命令查看支持的语言
+  ensure_install = {"json", "html", "css", "vim", "lua", "javascript", "c", "c++"},
+  --ensure_installed = "maintained"
+  --
+  --启用代码高亮模块
+  highlight = {
+     enable = true,
+     additional_vim_regex_hightlighting = false,
+  },
+  --启用增量选择模块
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "CR",
+      node_incremental = "CR",
+      node_decremental = "BS",
+      scope_incremental = "TAB",
+    },
+  },
+  --启用代码缩进模块 (=)
+  indent = {
+    enable = true
+  },
+  --p00f/nvim-ts-rainbow
+  rainbow = {
+    enble = true,
+    --disable +{"jsx", "cpp"} list of langurage you want to disable the plugin for
+    extended_mode = true, --Also hightlight non-bracket delimiters like html tags, boolean or table:lang->boolean
+    max_file_lines = nil, --Do not Eable for files with more than n lines int
+    -- 
+    --colors = {
+    --  "#95ca60",
+    --  "#ee6985",
+    --  "#D6A760",
+    --  "#7794F4",
+    --  "#B38BF5",
+    --  "#7CC7FE",
+   --},
+  }, --table of hex strings
+    --termcolors = {} talble of colour name strings
+})
+--开启folding 模块
+vim.opt .foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter #foldexpr()"
+--默认不要折叠
+--https://stackoveflow.com/questions/8316139/how-to-set-the-default-to-unfolded-when-you-open-a-file
+vim.opt.foldlevel = 99
